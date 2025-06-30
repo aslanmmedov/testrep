4d3d57b21a341045aa8629af6e5ad865c240403d
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-06-30 15:31:13 +0400

    commit

---
+++ src/components/Admin/Navbar/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { IoIosNotifications } from "react-icons/io";
+import icon from "../../assets/icon.avif"
+import { useTranslation } from 'react-i18next';
+function Navbar() {
+
+  const { t, i18n } = useTranslation();
+  const handleLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  }
+
+  return (
+    <div className='navbarr'>
+      <h2>  POS(Point of Sales) v3.0</h2>
+      <div className="navbar-right">
+        <select onChange={(e)=>handleLanguage(e.target.value)}>
+          <option value="az">az</option>
+           <option value="en">en</option>
+        </select>
+        <span className='time'>11 : 13 : 31 AM</span>
+        <span><IoIosNotifications /></span>
+        <div className="user-info">
+          <span className='user-name'>Admin</span>
+     
+            <img src={icon} alt="user avatar" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Admin/SideBar/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { NavLink } from "react-router";
+import { FaAppStore, FaChartLine, FaHome, FaUsers, FaUserSecret } from "react-icons/fa";
+import { BsFillCartCheckFill } from 'react-icons/bs';
+import { useTranslation } from 'react-i18next';
+
+
+function Sidebar() {
+const {t} = useTranslation();
+
+    return (
+        <div className='sidebarr'>
+            <h1>POS  {t("system")}</h1>
+            <ul>
+                <li>
+                    <NavLink to={""} end className={({ isActive }) => isActive ? "navlink active" : "navlink"} >   <FaHome />{ t("dashboard")}</NavLink>
+                </li>
+                <li>
+                    <NavLink to={"stores"} className={({ isActive }) => isActive ? "navlink active" : "navlink"}>
+                        <FaAppStore /> { t("stores")}</NavLink>
+                </li>
+                <li>
+                    <NavLink to={"users"} className={({ isActive }) => isActive ? "navlink active" : "navlink"}>
+                     <FaUsers /> { t("users")}
+                     </NavLink>
+                </li>
+                <li>
+                    <NavLink to={"suppliers"} className={({ isActive }) => isActive ? "navlink active" : "navlink"}><FaUserSecret /> { t("suppliers")}</NavLink>
+                </li>
+                <li>
+                    <NavLink to={"products"} className={({ isActive }) => isActive ? "navlink active" : "navlink"}> <FaChartLine /> { t("products")}</NavLink>
+                </li>
+                <li>
+                    <NavLink to={"orders"} className={({ isActive }) => isActive ? "navlink active" : "navlink"}> <BsFillCartCheckFill /> { t("orders")}</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
