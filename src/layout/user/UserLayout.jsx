f3fe382a55a093f2037e3201743798e519be6c96
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-06-30 12:42:57 +0400

    structure complated

---
+++ src/layout/user/UserLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+const UserLayout = () => {
+  return (
+    <>
+    <Header/>
+    {/* <Outlet/> */}
+    <Footer/>
+    </>
+  )
+}
+
+export default UserLayout
---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+//store file
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import apis from "./services";
+const middlewares = [];
+
+apis.forEach((api) => {
+  reducers[api.reducerPath] = api.reducer;
+  middlewares.push(api.middleware);
+});
+
+export const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(middlewares),
+});
+
+setupListeners(store.dispatch);
