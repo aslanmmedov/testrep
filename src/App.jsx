3aa874c90e02c0e6f1547e192bcb7ad043c461ff
aslanmmedov <aslanzm-azmp202@code.edu.az>
2025-06-30 13:01:45 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
-
+// import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+// import Routes from './router/router';
+// const routers = createBrowserRouter(Routes);
 function App() {
-  
-
   return (
-    <>
-      
-    </>
+      // <RouterProvider router = {routers}/>  
+      <>
+      </>
   )
 }
 
